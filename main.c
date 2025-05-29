caf6b4e4c3ad07e825f6dc5fbf5f8ffecc74a987
mukiibi <ibrahimmukiibi803@gmail.com>
2025-05-29 13:20:40 +0300

    LOGIC BUILDING WITH C PROGRAMMING

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+int age =20;
+float height=5.9;
+double distance= 12345.6789;
+char initial ='A';
+
+printf("The age is %d\n\n ",age);
+printf("The height is %f\n\n",height );
+printf("the distance is %.2lf\n\n",distance);
+printf("The initial is %c",initial);
+
+
+
+    return 0;
+}
